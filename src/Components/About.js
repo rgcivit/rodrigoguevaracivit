1c210bb02f96feeff0498f4f230442934770b90a
rgcivit <86114994+rgcivit@users.noreply.github.com>
2022-04-04 13:17:16 -0300

    merge

--- src/Components/About.js
+++ src/Components/About.js
@@ -74,7 +74,7 @@ const useStyles = makeStyles((theme) =>({
       margin:"0 auto",
      " & h3":{
       [theme.breakpoints.down("sm")]:{
-        fontSize:"1.5rem",
+        fontSize:"1rem",
       },
    
     },
