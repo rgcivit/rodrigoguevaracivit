ba1b4ab1fb523626c1e9ff5ee3d4eac380d76418
rgcivit <86114994+rgcivit@users.noreply.github.com>
2022-04-04 13:21:33 -0300

    change font title

--- src/Components/About.js
+++ src/Components/About.js
@@ -74,7 +74,7 @@ const useStyles = makeStyles((theme) =>({
       margin:"0 auto",
      " & h3":{
       [theme.breakpoints.down("sm")]:{
-        fontSize:"300px",
+        fontSize:"700px",
       },
    
      }
