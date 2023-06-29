a6f635f15d7cadf10ca5fe2c04789dcb81d31f42
rgcivit <86114994+rgcivit@users.noreply.github.com>
2023-06-28 23:59:19 -0300

    top2

--- src/Components/About.js
+++ src/Components/About.js
@@ -119,7 +119,7 @@ const useStyles = makeStyles((theme) =>({
       right:"4rem",
       [theme.breakpoints.down("sm")]:{
         bottom:"1rem",
-        top:0,
+        top:5,
         right:"3rem",
       },
       backgroundColor:"tomato",
