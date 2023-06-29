13952906ca8723f7923050a5e5e6c7aea8912e84
rgcivit <86114994+rgcivit@users.noreply.github.com>
2023-06-29 15:28:47 -0300

    ok

--- src/Components/About.js
+++ src/Components/About.js
@@ -121,7 +121,7 @@ const useStyles = makeStyles((theme) =>({
       top:"3rem",
       },
       backgroundColor:"tomato",
-      padding: theme.spacing(5),
+      padding: theme.spacing(3),
       "&:hover":{
         backgroundColor:"#fff",
 
