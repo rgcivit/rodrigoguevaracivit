106a55bc8f4248033d1eae5f2c7a737e4155d483
rgcivit <86114994+rgcivit@users.noreply.github.com>
2023-06-29 16:02:59 -0300

    ok

--- src/Components/About.js
+++ src/Components/About.js
@@ -142,7 +142,7 @@ const useStyles = makeStyles((theme) =>({
       right:"18rem",
       [theme.breakpoints.down("sm")]:{
        position:"absolute",
-       bottom:"5rem",
+       
        right:"7rem",
        
       },
