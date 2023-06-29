b6bdee8926de9e5c390ffd6bcb43fb52d5851402
rgcivit <86114994+rgcivit@users.noreply.github.com>
2023-06-29 15:48:33 -0300

    ok

--- src/Components/About.js
+++ src/Components/About.js
@@ -143,7 +143,7 @@ const useStyles = makeStyles((theme) =>({
       [theme.breakpoints.down("sm")]:{
        position:"fixed",
        
-       bottom:"5rem",
+       bottom:"7rem",
       },
       backgroundColor:"blue",
       padding: theme.spacing(3),
