f1aa0441289f337a9b75fcdd2367383bdd01af87
rgcivit <86114994+rgcivit@users.noreply.github.com>
2023-06-29 15:57:35 -0300

    ok

--- src/Components/About.js
+++ src/Components/About.js
@@ -144,7 +144,7 @@ const useStyles = makeStyles((theme) =>({
        position:"fixed",
        bottom:"5rem",
        right:"7rem",
-       left:"4rem",
+       left:"5rem",
       },
       backgroundColor:"blue",
       padding: theme.spacing(3),
