18d9b66c09927069624e0cbd004e098233dcddac
rgcivit <86114994+rgcivit@users.noreply.github.com>
2023-06-29 14:27:26 -0300

    epc

--- src/Components/About.js
+++ src/Components/About.js
@@ -137,7 +137,7 @@ const useStyles = makeStyles((theme) =>({
       }
     },
     pdfbutton2:{
-      position:"absolute",
+      position:"relative",
       bottom:"0.15rem",
       right:"18rem",
       [theme.breakpoints.down("sm")]:{
