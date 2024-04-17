e122f4dfc7360617cbae119134f08ff59b164d27
rgcivit <86114994+rgcivit@users.noreply.github.com>
2024-04-17 20:08:33 -0300

    poryecto tienda

--- src/mockData.js
+++ src/mockData.js
@@ -1,5 +1,6 @@
 import shareit from './images/shareit.jpeg'
 import videogames2 from './images/videogames2.jpeg'
+import tiendaselecta from './images/tiendaselecta.jpg'
 
 
 
@@ -15,7 +16,11 @@ const mockData =[
         link:"https://pi-videogames-five.vercel.app/Home"
         
     },
-    
+     {   title:"Tienda Bioselecta",
+        image:tiendaselecta,
+        link:"https://tienda-selecta.web.app/"
+
+    }
 ]
 
 export default mockData;
